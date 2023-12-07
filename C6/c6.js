9d1a0b0f952786de221b0928b96f1bee89386587
Nic Taylor <NTaylor1993@live.co.uk>
2023-12-07 07:13:40 +0000

    C6 tweaks

--- C6/c6.js
+++ C6/c6.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const inputFile = fs.readFileSync("C6/input-test.txt", "utf-8");
+const inputFile = fs.readFileSync("C6/input.txt", "utf-8");
 const inputStrings = inputFile.split("\n");
 
 const data = {};
@@ -26,26 +26,37 @@ const calculateIsWin = (speed, remainingTime, expectedDistance) => {
     return speed * remainingTime > expectedDistance;
 };
 
-const p1 = ({ time, distance }) => {
-    console.log({ time, distance });
+const calculateWinCount = (time, expectedDistance) => {
+    let winCount = 0;
 
+    for (let i = 0; i <= time; i++) {
+        const { speed, remainingTime } = calculateSpeedAndRemainingTime(i, time, 1);
+        if (calculateIsWin(speed, remainingTime, expectedDistance)) {
+            winCount++;
+        }
+    }
+
+    return winCount;
+};
+
+const p1 = ({ time, distance }) => {
     const winCounts = [];
 
     time.forEach((time, index) => {
         const expectedDistance = distance[index];
 
-        let winCount = 0;
-
-        for (let i = 0; i <= time; i++) {
-            const { speed, remainingTime } = calculateSpeedAndRemainingTime(i, time, 1);
-            if (calculateIsWin(speed, remainingTime, expectedDistance)) {
-                winCount++;
-            }
-        }
-        winCounts.push(winCount);
+        winCounts.push(calculateWinCount(time, expectedDistance));
     });
 
     return winCounts.reduce((a, b) => a * b, 1);
 };
 
-p1(data);
+const p2 = ({ time, distance }) => {
+    const actualTime = Number(time.reduce((a, b) => a + b, ""));
+    const actualDistance = Number(distance.reduce((a, b) => a + b, ""));
+
+    return calculateWinCount(actualTime, actualDistance);
+};
+
+console.log(p1(data));
+console.log(p2(data));

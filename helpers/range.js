a1b9454bd4119e0cc98860320cd5730eaa29180d
Nic Taylor <NTaylor1993@live.co.uk>
2023-12-07 08:12:43 +0000

    Delete unused helpers

--- helpers/range.js
+++
@@ -1,4 +0,0 @@
-export const range = (...args) => definedRange(args[1] ? args[0] : 0, args[1] ?? args[0], args[2]);
-
-const definedRange = (start, stop, step = 1) =>
-    [...Array(Math.ceil((stop - start) / step))].map((_, i) => i * step + start);

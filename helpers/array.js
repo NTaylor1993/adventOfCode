bce28ea7d398c905251a3beaf1e28051ae6e51a9
Nic Taylor <NTaylor1993@live.co.uk>
2023-12-09 07:02:21 +0000

    Helper functions

---
+++ helpers/array.js
@@ -0,0 +1,3 @@
+export const last = (arr) => arr[arr.length - 1];
+
+export const sum = (arr) => arr.reduce((a, b) => a + b, 0);
---
+++ helpers/math.js
@@ -0,0 +1,3 @@
+const gcd = (a, b) => (a ? gcd(b % a, a) : b);
+
+export const lcm = (a, b) => (a * b) / gcd(a, b);
